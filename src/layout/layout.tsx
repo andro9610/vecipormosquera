2ad473cc03b3503325771fd9d6ede19176df74fe
andro9610 <36451824+andro9610@users.noreply.github.com>
2026-07-31 16:30:36 -0500

    Carga de ajustes para permitir la compilacion

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,63 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MaterialIcon } from "../fragments/materialIcon";
+import { MaterialIcon } from "../fragments/MaterialIcon";
 import { Partners } from "./partners";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
-import {
-  ArrowUpRight,
-  Play,
-  ShieldCheck,
-  User,
-  ArrowDown,
-  Sparkles,
-  Share2,
-  Triangle,
-  Hexagon,
-  Layers,
-} from "lucide-react";
-
-interface NodePointProps {
-  name: string;
-  value: string;
-  position: string;
-  icon: React.ReactNode;
-  iconPosition?: "left" | "right";
-}
-
-const NodePoint: React.FC<NodePointProps> = ({
-  name,
-  value,
-  position,
-  icon,
-  iconPosition = "left",
-}) => {
-  return (
-    <div
-      className={`absolute ${position} flex items-center space-x-3 text-xs text-slate-500 z-10`}
-    >
-      {iconPosition === "left" && (
-        <div className="w-8 h-8 rounded-full bg-white border border-slate-200 flex items-center justify-center text-slate-700 shadow-lg backdrop-blur-md">
-          {icon}
-        </div>
-      )}
-      <div className="flex flex-col">
-        <div className="flex items-center space-x-1">
-          <span className="w-1.5 h-1.5 rounded-full bg-slate-400 inline-block"></span>
-          <span className="font-semibold text-slate-700">{name}</span>
-        </div>
-        <span className="text-[10px] text-slate-500 pl-2.5">{value}</span>
-      </div>
-      {iconPosition === "right" && (
-        <div className="w-8 h-8 rounded-full bg-white border border-slate-200 flex items-center justify-center text-slate-700 shadow-lg backdrop-blur-md">
-          {icon}
-        </div>
-      )}
-    </div>
-  );
-};
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
@@ -78,7 +26,8 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
 
             <div className="flex items-center space-x-2">
               <button className="btn btn-outline my-2 ms-1 me-5 flex items-center gap-1">
-                Contactanos <ArrowUpRight className="w-3 h-3 ml-1" />
+                Contactanos
+                <MaterialIcon icon="arrow_outward" />
               </button>
             </div>
           </header>
